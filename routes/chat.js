18c5a6fb0b96d8bbe76555ed35342bec516489b2
Erewhon-Lucy <1663523296@qq.com>
2023-08-22 17:20:01 -0700

    setup chat route

--- routes/chat.js
+++ routes/chat.js
@@ -1,9 +1,9 @@
 var express = require('express');
-const { render } = require('pug');
 var router = express.Router();
 
-router.get('/chat', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('chat');
-});
+
+})
 
 module.exports = router;
