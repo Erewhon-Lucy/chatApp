e08b7680e9732cff2147b6d8faadae55962e8fa2
Erewhon-Lucy <1663523296@qq.com>
2023-08-22 21:54:16 -0700

    edit: index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,7 +1,29 @@
 $(() => {
-    console.log('jquery loaded');
+    console.log('page loaded');
+
     $("#show-register-form").on("click", function() {
         $(".login-form").hide();
         $(".register-form").show();
+    });
+
+    $("form.login-form").on("submit", function(event) {
+        event.preventDefault();
+        console.log('login form submitted');
+        var username = $("#username").val();
+        var password = $("#password").val();
+
+        $.post("/login", { username, password }, function(data) {
+            // 处理响应，可能需要重定向或显示消息
+        });
+    })
+
+    $("form.register-form").on("submit", function(event) {
+        event.preventDefault();
+        const newUsername = $("#new-username").val();
+        const newPassword = $("#new-password").val();
+        
+        $.post("/register", { newUsername, newPassword }, function(data) {
+            // 处理响应，可能需要重定向或显示消息
+        });
     });
 })
