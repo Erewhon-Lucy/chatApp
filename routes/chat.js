78de9a70c3b926e8943893b3b5fa373f2ed08574
Erewhon-Lucy <1663523296@qq.com>
2023-09-02 18:19:21 -0700

    before excepting io entity

--- routes/chat.js
+++ routes/chat.js
@@ -2,18 +2,19 @@ var express = require('express');
 var router = express.Router();
 const ChatMessage = require('../models/chatMessage');
 const { isObjectIdOrHexString } = require('mongoose');
-const io = require('socket.io')();
 
 io.on("connection", (socket) => {
-  	console.log("socket connected");
+  	console.log("server socket connected");
 	socket.on("add-message", async (message) => {
 		try {
-            // 获取最新添加到数据库的一条消息
-            const latestMessage = await ChatMessage.findOne().sort({ timestamp: -1 }).lean();
-            if (latestMessage) {
-                // 广播给所有客户端
-                io.emit('add-message', latestMessage);
-            }
+            const { username, message: msg, timestamp } = message;
+            const chatMessage = new ChatMessage({ username, message: msg, timestamp });
+            await chatMessage.save();
+            io.emit('add-message', {
+                username: username,
+                message: msg,
+                timestamp: chatMessage.timestamp
+            });
         } catch (err) {
             console.error(err);
         }
@@ -39,8 +40,6 @@ router.post('/send', async (req, res) => {
 		const { username, message } = req.body;
 		const chatMessage = new ChatMessage({ username, message });
 		await chatMessage.save();
-
-		io.emit('add-message');
 
 		res.json({ success: true });
 
